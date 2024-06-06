d60c3cdc333932373c06e89c169f31bf604d6ef8
KhirubaSangkari <khiruba@plentisoft.com>
2024-06-06 14:17:47 +0800

    Added Node.js, Express.js and Firebase

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+// app.use('/api/users', require('./routes/userRoutes'));
+
+app.get('/', (req, res) => {
+res.send('Hello World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
