670789d1ec5f8307b9317c5262e35b929936da79
KhirubaSangkari <khiruba@plentisoft.com>
2024-06-07 02:26:34 +0800

    added some css changes to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerGreen: 'rgba(131, 225, 11, 0.22)',
+        btnGreen: '#00A359',
+      },
+    },
   },
   plugins: [],
 }
