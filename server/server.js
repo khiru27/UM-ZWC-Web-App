ab17fd511f677060d35a8a6091d2443dc3a96764
KhirubaSangkari <khiruba@plentisoft.com>
2024-06-11 02:31:31 +0800

    added configuration for server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 
-// Routes
-// app.use('/api/users', require('./routes/userRoutes'));
+// Serve static files from the 'src' directory
+app.use(express.static(path.join(__dirname, '..', 'src')));
 
+// Serve static files from the 'server' directory
+app.use(express.static(path.join(__dirname, '..')));
+
+// Handle root route
 app.get('/', (req, res) => {
-res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
